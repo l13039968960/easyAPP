d068d501afcdbbd01c909f4c7f573e68a120d220
l13039968960 <l13039968960@163.com>
2026-09-08 20:41:57 +0800

    code event.c: delete eventbuffer

--- inc/easyapp_event.h
+++ inc/easyapp_event.h
@@ -5,7 +5,6 @@
 #include <stddef.h>
 #include "easyapp_page.h"
 
-#define event_buffer_nums 256
 #define event_queue_nums 128
 
 typedef struct easyapp_event
@@ -21,14 +20,10 @@ typedef struct easyapp_eventqueue
     uint32_t Head;
     uint32_t Tail;
 
-    easyapp_event_t* eventqueue[event_queue_nums];
+    easyapp_event_t eventqueue[event_queue_nums];
 }easyapp_eventqueue_t;
 
-easyapp_event_t* sp_easyapp_event_create(EASYAPP_RIGISTERED_EVENTS_t event_id, uint32_t event_flags, void *event_data);
-
-int32_t x_easyapp_event_delete(easyapp_event_t* event);
-
-int32_t x_easyapp_event_send(easyapp_event_t *event);
+int32_t x_easyapp_event_send(EASYAPP_RIGISTERED_EVENTS_t event_id, uint32_t event_flags, void *event_data);
 
 easyapp_event_t* sp_easyapp_event_get(void);
 
--- src/easyapp_event.c
+++ src/easyapp_event.c
@@ -1,70 +1,29 @@
 #include "../inc/easyapp_event.h"
 
-static easyapp_event_t event_buffer[event_buffer_nums];
-static uint32_t event_buffer_idx;
-
-easyapp_eventqueue_t eventqueue;
+static easyapp_eventqueue_t eventqueue;
 
 easyapp_event_t* sp_easyapp_event_get(void)
 {
     if(eventqueue.Head == eventqueue.Tail)
-        return NULL; //FULL
+        return NULL; //NULL
 
-     easyapp_event_t* event = eventqueue.eventqueue[eventqueue.Head];
+     easyapp_event_t* event = &eventqueue.eventqueue[eventqueue.Head];
+
      eventqueue.Head++;
-
      if(eventqueue.Head == event_queue_nums)
         eventqueue.Head = 0;
 
     return event;
 }
 
-easyapp_event_t* sp_easyapp_event_create(EASYAPP_RIGISTERED_EVENTS_t event_id, uint32_t event_flags, void *event_data)
+int32_t x_easyapp_event_send(EASYAPP_RIGISTERED_EVENTS_t event_id, uint32_t event_flags, void *event_data)
 {
-    if(event_id <= 0 )
-        return NULL;
-
-    uint32_t count = 0;
-
-    while(event_buffer[event_buffer_idx].event_id != 0)
-    {
-        event_buffer_idx++;
-        if(event_buffer_idx == event_buffer_nums)
-            event_buffer_idx = 0;
-
-        count++;
-        if(count == event_buffer_nums)
-            return NULL;
-    }
-
-    event_buffer[event_buffer_idx].event_id = event_id;
-    event_buffer[event_buffer_idx].event_flags = event_flags;
-    event_buffer[event_buffer_idx].event_data = event_data;
-
-    return &event_buffer[event_buffer_idx];
-}
-
-int32_t x_easyapp_event_delete(easyapp_event_t* event)
-{
-    if(event == NULL)
-        return -1;
-
-    event->event_id = 0;
-    event->event_data = 0;
-    event->event_flags = 0;
-
-    return 0;
-}
-
-int32_t x_easyapp_event_send(easyapp_event_t *event)
-{
-    if(event == NULL)
-        return -1;
-
     if((eventqueue.Tail + 1) % event_queue_nums == eventqueue.Head)
-        return -2;
+        return -1; //FULL
 
-    eventqueue.eventqueue[eventqueue.Tail] = event;
+    eventqueue.eventqueue[eventqueue.Tail].event_id = event_id;
+    eventqueue.eventqueue[eventqueue.Tail].event_flags = event_flags;
+    eventqueue.eventqueue[eventqueue.Tail].event_data = event_data;
 
     eventqueue.Tail++;
     if(eventqueue.Tail == event_queue_nums)
